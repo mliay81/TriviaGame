56dbb8afb0edc85b88d64fe897d5b2d7e9e5eb8b
unknown <mikeliay@gmail.com>
2018-06-10 01:33:44 -0500

    bulk of html and beginning of js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,53 @@
 
-// $(document).ready(function () {
-
-  
-// var counter = 30;
-// var interval = setInterval(function() {
-//     counter--;
-//     console.log(counter)
-//     $(".card-title").html("<h2>About 10 seconds left!</h2>")
-//     // Display 'counter' wherever you want to display it.
-//     if (counter == 0) {
-//         alert("Time's Up!")
-//         // Display a login box
-//         clearInterval(interval);
-//     }
-// }, 1000);
-// // function myFunction() {
-// //     document.getElementById("demo").innerHTML = "hello"
-// //     console.log("hello")
-// // }
-
-// // ("button").on("click", myFunction)
 
 
-// });
+var timer = 120
+var correct = 0
+var incorrect = 0
+var unanswered = 0
+
+
+
+
+
+
+
+
+questions = {
+    q1: "Which Twin Cities band scored the one-hit wonder 'Surfin' Bird?'",
+    q2: "Who was the original drummer for The Replacements?",
+    q3: "Sean Tillmann also performed under what moniker, besides Har Mar Superstar?",
+    q4: "Craig Finn fronted which band before The Hold Stead?",
+    q5: "In what Minnesota city was Bob Dylan born?",
+    q6: "Prince's album Purple Rain was recorded live at which Minneapolis landmark?",
+    q7: "Tony Zaccardi just purchased which long-standing Twin Cities dive bar?",
+    q8: "Which local 90's band took their name from the movie 'It's A Wonderful Life?'?",
+    q9: "What is the name of Morris Day's valet?",
+    q10: "A new local hip hop group is named after which comic book villain?"
+};
+
+options = {
+    q1: ["Husker Du", "The Time", "The Trashmen", "Lizzo"],
+    q2: ["Chris Mars", "Ringo Starr", "Bob Mould","John Blackwell Jr."],
+    q3: ["Night Moves", "The Horrors", "The Suburbs", "Sean Na Na"],
+    q4: ["Lftr Pllr", "The Revolution", "Cut Copy", "Koo Koo Kangaroo"],
+    q5: ["Hibbing", "St. Paul", "Duluth", "Ely"],
+    q6: ["The Metrodome", "First Avenue", "The Mall of America", "The Turf Club"],
+    q7: ["Palmer's", "Grumpy's", "The Triple Rock", "Vegas Lounge"],
+    q8: ["Soul Asylum", "Babes In Toyland", "Zuzu's Petals", "Dillinger Four"],
+    q9: ["Bobby Stinson", "Jerome Benton", "Dan Wilson", "Lori Barbero"],
+    q10: ["The Joker", "Shredder", "Kingpin", "Lex Luthor"]
+}
+
+answers = {
+    q1: "The Trashmen",
+    q2: "Chris Mars",
+    q3: "Sean Na Na",
+    q4: "Lftr Pllr",
+    q5: "Duluth",
+    q6: "First Avenue",
+    q7: "Palmer's",
+    q8: "Zuzu's Petals",
+    q9: "Jerome Benton",
+    q10: "Shredder"
+}
