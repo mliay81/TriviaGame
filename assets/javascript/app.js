16ac5fa0c0fab4abe00c473c91b19d78355264a1
unknown <mikeliay@gmail.com>
2018-06-09 22:18:45 -0500

    first draft

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+
+// $(document).ready(function () {
+
+  
+// var counter = 30;
+// var interval = setInterval(function() {
+//     counter--;
+//     console.log(counter)
+//     $(".card-title").html("<h2>About 10 seconds left!</h2>")
+//     // Display 'counter' wherever you want to display it.
+//     if (counter == 0) {
+//         alert("Time's Up!")
+//         // Display a login box
+//         clearInterval(interval);
+//     }
+// }, 1000);
+// // function myFunction() {
+// //     document.getElementById("demo").innerHTML = "hello"
+// //     console.log("hello")
+// // }
+
+// // ("button").on("click", myFunction)
+
+
+// });
